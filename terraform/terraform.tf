85363abfee99dd815ee9a3c401fcbee9f4413dfa
Yasssh-bombale <yashbombale@gmail.com>
2026-05-30 13:11:34 +0530

    add s3 backend for terraform.tfstate

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,13 @@
 terraform {
+   backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket-yash-123"
+    key    = "s3-backend"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "6.38.0"
     }
   }
 }
